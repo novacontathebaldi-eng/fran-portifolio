8e5e33a2ac9792f4acdc4eb03d35566df0030dbe
othebaldi <compras.thebaldi@gmail.com>
2025-12-10 15:54:48 -0300

    att volta

--- pages/ClientArea.tsx
+++ pages/ClientArea.tsx
@@ -741,7 +741,7 @@ export const ClientArea: React.FC = () => {
                 {/* Address Modal */}
                 <AnimatePresence>
                   {showAddressModal && (
-                    <div className="fixed inset-0 bg-black/50 z-[9999] flex items-center justify-center p-4">
+                    <div className="fixed inset-0 bg-black/50 z-[100] flex items-center justify-center p-4">
                       <motion.div
                         initial={{ scale: 0.9, opacity: 0 }}
                         animate={{ scale: 1, opacity: 1 }}
